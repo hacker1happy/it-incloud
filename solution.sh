b13e99e8018994373b3cba1f695ebf562dc2da43
hacker1happy <9547priyanshu.gupta@gmail.com>
2022-04-07 02:05:46 +0530

    changed solution

--- solution.sh
+++ solution.sh
@@ -10,29 +10,43 @@ do
     if [[ $line == *"tls.crt"* ]]
     then
         cert=1
+        IFS=':' #setting space as delimiter  
+        read -ra ADDR <<<"$line" #reading str as an array as tokens separated by IFS  
+        echo ${ADDR[2]} >> tls.crt
         continue
     fi
+
     if [[ $cert == 1 ]]
     then
-        if [[ $line == *"ending"* ]]
+        if [[ $line == *-*[A-Z]*-* ]]
         then
             cert=0
+            if [[ $line == *"tls.key"* ]]
+            then
+                key=1
+                IFS=':' #setting space as delimiter  
+                read -ra arr <<<"$line" #reading str as an array as tokens separated by IFS 
+                echo ${arr[1]} >> tls.key
+
+                IFS='tls.key'
+                read -ra ADDR <<<"${arr[0]}" 
+                echo ${ADDR[0]} >> tls.crt
+                continue
+            fi
         fi
         echo $line >> tls.crt
     fi
 
-    ## if line contains "BEGIN KEY"/"metadata with tls.key" then we are to in the key file
-    if [[ $line == *"tls.key"* ]]
-    then
-        key=1
-        continue
-    fi
     if [[ $key == 1 ]]
     then
-        if [[ $line == *"ending"* ]]
-        then
-            key=0
-        fi
+            if [[ $line == *"END OPENSSH TESTKEY"* ]]
+            then
+                key=0
+                IFS=']' #setting space as delimiter  
+                read -ra arr <<<"$line" #reading str as an array as tokens separated by IFS 
+                echo ${arr[0]} >> tls.key
+                continue
+            fi
         echo $line >> tls.key
     fi
 done < cert.txt
